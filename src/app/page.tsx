fde1edba13d4e6bdbd4fdd52d57ad112d863e8ea
conejofoz <conejofoz@gmail.com>
2025-01-28 21:33:41 -0300

    usando o zod no formulário

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,44 @@
 "use client"
 
 import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
+/* pode ficar fora do componente ou em outro arquivo */
+const SignUpFormShema = z.object({
+  name: z.string().min(2).max(20),
+  lastName: z.string().min(2).optional(), //no zod por padrão tudo é obrigatório
+  age: z.number().min(18)
+});
 
 
 const Page = ()=>{
 
-  type SignUpForm = {
-    name: string;
-    lastName: string;
-    age: number;
-  }
+  const {register, handleSubmit } = useForm({resolver: zodResolver(SignUpFormShema)});
 
-  const SignUpForm = z.object({
-    name: z.string().min(2).max(20),
-    lastName: z.string().min(2).optional(), //no zod por padrão tudo é obrigatório
-    age: z.number().min(18)
-  });
-
-  
-  /* Inferindo um tipo pelo zod */
-  type SigUpObject = z.infer<typeof SignUpForm>;
-
-  const obj: SigUpObject = {
-    name: "Xivio",
-    lastName: "conejo",
-    age: 90
+  const handleSignUpForm = ()=>{
+    alert('vai enviar')
   }
 
   return (
     <div className="container mx-auto">
+
+      <form onSubmit={handleSubmit(handleSignUpForm)}>
+        <input 
+          {...register('name')}
+          className='border p-3 m-3'
+        />
+        <input 
+          {...register('lastName')}
+          className='border p-3 m-3'
+        />
+        <input 
+          {...register('age', {valueAsNumber: true})}
+          className='border p-3 m-3'
+        />
+
+        <input type="submit" value="Cadastrar" />
+      </form>
 
       
 
