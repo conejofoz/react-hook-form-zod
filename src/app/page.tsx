ddc809d123ab35e6b66644ed86647fe6b65a29e5
conejofoz <conejofoz@gmail.com>
2025-01-28 21:50:03 -0300

    definindo mensagens de erro no zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,14 @@ import { zodResolver } from '@hookform/resolvers/zod';
 const SignUpFormShema = z.object({
   name: z.string().min(2).max(20),
   lastName: z.string().min(2).optional(), //no zod por padrão tudo é obrigatório
-  age: z.number().min(18)
+  email: z.string().email(),
+  age: z.number({invalid_type_error: 'Idade precisa ser um número'}).min(18, {message: 'Precisa ser maior de 18 anos.'})
 });
 
 
 const Page = ()=>{
 
-  const {register, handleSubmit } = useForm({resolver: zodResolver(SignUpFormShema)});
+  const {register, handleSubmit, formState: {errors} } = useForm({resolver: zodResolver(SignUpFormShema)});
 
   const handleSignUpForm = ()=>{
     alert('vai enviar')
@@ -24,18 +25,36 @@ const Page = ()=>{
     <div className="container mx-auto">
 
       <form onSubmit={handleSubmit(handleSignUpForm)}>
-        <input 
-          {...register('name')}
-          className='border p-3 m-3'
-        />
-        <input 
-          {...register('lastName')}
-          className='border p-3 m-3'
-        />
-        <input 
-          {...register('age', {valueAsNumber: true})}
-          className='border p-3 m-3'
-        />
+        <div>
+          <input 
+            {...register('name')}
+            className='border p-3 m-3'
+          />
+          {errors.name && <p>{errors.name.message as string}</p>}
+        </div>
+        <div>
+          <input 
+            {...register('lastName')}
+            className='border p-3 m-3'
+          />
+          {errors.lastName && <p>{errors.lastName.message as string}</p>}
+
+        </div>
+        <div>
+          <input 
+            {...register('email')}
+            className='border p-3 m-3'
+          />
+          {errors.email && <p>{errors.email.message as string}</p>}
+
+        </div>
+        <div>
+          <input 
+            {...register('age', {valueAsNumber: true})}
+            className='border p-3 m-3'
+          />
+          {errors.age && <p>{errors.age.message as string}</p>}
+        </div>
 
         <input type="submit" value="Cadastrar" />
       </form>
